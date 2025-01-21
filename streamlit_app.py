b7fff4e26c543b3bef6dc9d6b8597bd1be4d1e0d
CODEAbhinav-art <abhinav15102003@gmail.com>
2025-01-21 22:37:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,10 +39,10 @@ data={'island':island,
 
 input_df=pd.DataFrame(data,index=[0])
 input_penguins=pd.concat([input_df,X],axis=0)
-input_df
+
 
 with st.expander('Input feautures'):
-   st.write('**Input Penguins')
+   st.write('**Input Penguins**')
    input_df
    st.write('**combined penguins data**')
    input_penguins
