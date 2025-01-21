211a16430195beb169b2d5611ecc75120e7b201c
CODEAbhinav-art <abhinav15102003@gmail.com>
2025-01-21 14:45:11 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title('machine learning app')
 
 st.info('its an amazing ml app created from technocrats project')
+df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/palmer-penguins/refs/heads/master/data/penguins_cleaned.csv')
