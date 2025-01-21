b82cb1ce65194de82959db031a7dfb43c6b19137
CODEAbhinav-art <abhinav15102003@gmail.com>
2025-01-21 21:14:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 
-st.title('ABHINAVs machine learning app')
+st.title('ABHINAVs machine learning app😎')
 st.info('My first ML project for_the TECHNOCRATS CLUB')
 
 with st.expander('Data'):
@@ -15,4 +15,20 @@ with st.expander('Data'):
    st.write('**y**')
    Y=df.species
    Y
+
+with st.expander('Data Visualization'):
+   st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',color='species')
+
+#Data preparations
+with st.sidebar:
+st.header('Input features')
+island=st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+bill_length_mm=st.slider('Bill length(mm)',32.1,59.6,43.9)
+bill_depth_mm=st.slider('Bill Depth(mm)',13.1,21.5,17.2)
+flipper_length_mm=st.slider('Flipper Length(mm)',172.0,231.0,201.0)
+body_mass_g=st.slider('Body Mass(gm)'2700.0,6300.0,4207.0)
+gender=st.selectbox('Gender',('Male','Female'))
+
+#create a DAtaframe for the input features
+
 
