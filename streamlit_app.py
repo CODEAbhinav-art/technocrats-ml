e47f9bb1aa7f6122edef44009c3357c0cfea52d7
CODEAbhinav-art <abhinav15102003@gmail.com>
2025-01-21 14:48:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 
 st.title('machine learning app')
-
 st.info('its an amazing ml app created from technocrats project')
-df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/palmer-penguins/refs/heads/master/data/penguins_cleaned.csv')
-df
+
+with st.expander('Data'):
+   st.write('**Raw data**')
+   df=pd.read_csv('https://raw.githubusercontent.com/dataprofessor/palmer-penguins/refs/heads/master/data/penguins_cleaned.csv')
+   df
