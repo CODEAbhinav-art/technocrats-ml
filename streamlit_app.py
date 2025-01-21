82441a0971831573a12d236b470b6d399c27cbe5
CODEAbhinav-art <abhinav15102003@gmail.com>
2025-01-21 21:16:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,15 @@ with st.expander('Data Visualization'):
 
 #Data preparations
 with st.sidebar:
-st.header('Input features')
-island=st.selectbox('Island',('Biscoe','Dream','Torgersen'))
-bill_length_mm=st.slider('Bill length(mm)',32.1,59.6,43.9)
-bill_depth_mm=st.slider('Bill Depth(mm)',13.1,21.5,17.2)
-flipper_length_mm=st.slider('Flipper Length(mm)',172.0,231.0,201.0)
-body_mass_g=st.slider('Body Mass(gm)'2700.0,6300.0,4207.0)
-gender=st.selectbox('Gender',('Male','Female'))
-
+   st.header('Input features')
+   island=st.selectbox('Island',('Biscoe','Dream','Torgersen'))
+   bill_length_mm=st.slider('Bill length(mm)',32.1,59.6,43.9)
+   bill_depth_mm=st.slider('Bill Depth(mm)',13.1,21.5,17.2)
+   flipper_length_mm=st.slider('Flipper Length(mm)',172.0,231.0,201.0)
+   body_mass_g=st.slider('Body Mass(gm)'2700.0,6300.0,4207.0)
+   gender=st.selectbox('Gender',('Male','Female'))
+   
+# ddcde
 #create a DAtaframe for the input features
 
 
