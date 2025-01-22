54dc6c7e953f96d1ecba38c7844eb7d83f582edd
CODEAbhinav-art <abhinav15102003@gmail.com>
2025-01-22 14:22:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,14 +61,15 @@ target_mapper={
 }
 def target_encode(val):
    return target_mapper(val)
-y=y_raw.apply(target_encode)
+   
+y=Y_raw.apply(target_encode)
 y
-y_raw
+Y_raw
 
 with st.expander('**Data Preparations**'):
    st.write('**Encoded X (input penguin)**')
    input_row
    st.write('**Encoded Y (input penguin)**')
-   y
+   
 
 
