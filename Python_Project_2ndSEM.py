595a33514b8112289ee59da0aa5e72dcf7e168a5
Abhinav Mishra <abhinav15102003@gmail.com>
2025-03-15 18:46:02 +0530

    Update Python_Project_2ndSEM.py

--- Python_Project_2ndSEM.py
+++ Python_Project_2ndSEM.py
@@ -1,30 +1,9 @@
-import pickle
-from pathlib import Path
 
 import streamlit as st
 import pandas as pd
 import streamlit_authenticator as Stauth
 
-# Login system
-names=['admin','user']
-usernames=['admin','user']
 
-# load hashed passwords
-file_path=Path(__file__).parent/"hashed_pw.pkl"
-with file_path.open("rb") as file:
-  hashed_passwords=pickle.load(file)
-
-authenticator=Stauth.Authenticator(names,usernames,hashed_passwords,"sales_dashboard","abcdef",cookie_expiry_days=30)
-
-name,authentication_status,username=authenticator.login("login","main")
-
-if authentication_status==False:
-    st.error("username/pw is incorrect")
-    
-if authentication_status==None:
-    st.warning("please enter your name and password")
-
-if authentication_status:
     # Custom CSS for styling
      st.markdown(
          """
