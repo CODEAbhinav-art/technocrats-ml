9add649a23361c54cf9f91a6fdd33273948969ae
Abhinav Mishra <abhinav15102003@gmail.com>
2025-03-15 15:45:44 +0530

    Update Python_Project_2ndSEM.py

--- Python_Project_2ndSEM.py
+++ Python_Project_2ndSEM.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 import streamlit as st
 import pandas as pd
-import streamlit-authenticator as Stauth
+import streamlit_authenticator as Stauth
 
 # Login system
 names=['admin','user']
